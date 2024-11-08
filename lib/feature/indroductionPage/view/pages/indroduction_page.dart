e15b9c8d9b20d798e5ee623b9dec32093dedf470
AdnanAdu <adnanpe5@gmail.com>
2024-11-08 11:43:23 +0530

    refactored three intro pages

--- lib/feature/indroductionPage/view/pages/indroduction_page.dart
+++ lib/feature/indroductionPage/view/pages/indroduction_page.dart
@@ -13,6 +13,43 @@ class IndroductionPage extends HookWidget {
       currentPage.value = index;
     }
 
+    bool isLastPage() {
+      return currentPage.value == 2;
+    }
+
+    List<Widget> buildPageViewChildren() {
+      return const [
+        OnboardingPage(
+          imagePath: 'assets/images/villa_intro_1.jpg',
+          title: 'Thousands of the best real estate at affordable prices',
+        ),
+        OnboardingPage(
+          imagePath: 'assets/images/villa_intro_2.jpg',
+          title: 'Book a real estate quickly and easily with one click',
+        ),
+        OnboardingPage(
+          imagePath: 'assets/images/villa_intro_3.jpeg',
+          title: 'Let\'s find the real estate that suits you right now!',
+        ),
+      ];
+    }
+
+    List<Widget> buildPageIndicators() {
+      return List.generate(3, (index) {
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          margin: const EdgeInsets.symmetric(horizontal: 5),
+          width: currentPage.value == index ? 30 : 8,
+          height: 8,
+          decoration: BoxDecoration(
+            color: currentPage.value == index ? Colors.blue : Colors.grey,
+            // shape: BoxShape.circle,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        );
+      });
+    }
+
     return Scaffold(
       body: Column(
         children: [
@@ -20,41 +57,40 @@ class IndroductionPage extends HookWidget {
             child: PageView(
               controller: pageController,
               onPageChanged: onPageChanged,
-              children: const [
-                OnboardingPage(
-                  imagePath: 'assets/images/villa_intro_1.jpg',
-                  title:
-                      'Thousands of the best real estate at affordable prices',
-                ),
-                OnboardingPage(
-                  imagePath: 'assets/images/villa_intro_2.jpg',
-                  title: 'Book a real estate quickly and easily with one click',
-                ),
-                OnboardingPage(
-                  imagePath: 'assets/images/villa_intro_3.jpeg',
-                  title:
-                      'Let\'s find the real estate that suits you right now!',
-                  isLastPage: true,
-                ),
-              ],
+              children: buildPageViewChildren(),
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(3, (index) {
-              return AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                margin: const EdgeInsets.symmetric(horizontal: 5),
-                width: currentPage.value == index ? 12 : 8,
-                height: 8,
-                decoration: BoxDecoration(
-                  color: currentPage.value == index ? Colors.blue : Colors.grey,
-                  shape: BoxShape.circle,
-                ),
-              );
-            }),
+            children: buildPageIndicators(),
           ),
           const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (isLastPage()) {
+                  // Navigate to home screen or main app page
+                } else {
+                  pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeIn,
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 56),
+                foregroundColor: Colors.white,
+                backgroundColor: const Color(0xff246afd),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              ),
+              child: Text(isLastPage() ? 'Get Started' : 'Next'),
+            ),
+          ),
         ],
       ),
     );
@@ -64,23 +100,29 @@ class IndroductionPage extends HookWidget {
 class OnboardingPage extends StatelessWidget {
   final String imagePath;
   final String title;
-  final bool isLastPage;
 
   const OnboardingPage({
     super.key,
     required this.imagePath,
     required this.title,
-    this.isLastPage = false,
   });
 
   @override
   Widget build(BuildContext context) {
+    // final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Column(
+      // crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Image.asset(imagePath, fit: BoxFit.cover),
+          child: Image.asset(
+            imagePath,
+            fit: BoxFit.cover,
+            height: screenHeight * 0.6,
+            width: double.infinity,
+          ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 32.0),
@@ -92,26 +134,6 @@ class OnboardingPage extends StatelessWidget {
               fontWeight: FontWeight.bold,
               color: Colors.blue,
             ),
-          ),
-        ),
-        const Spacer(),
-        Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: ElevatedButton(
-            onPressed: () {
-              if (isLastPage) {
-                // Navigate to home screen or main app page
-              } else {
-                // Navigate to the next page
-              }
-            },
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-            ),
-            child: Text(isLastPage ? 'Get Started' : 'Next'),
           ),
         ),
       ],
